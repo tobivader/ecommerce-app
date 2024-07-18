53b6c4bc819c160db4e0dad8fae881aa4b71301b
tobivader <77734926+tobivader@users.noreply.github.com>
2024-07-17 22:28:41 -0400

    fixed the picture size and Desserts title to align with it.

--- src/Pages/Desserts.js
+++ src/Pages/Desserts.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Products from '../assests/Product';
-import './Desserts.css'
+import React from "react";
+import Products from "../assests/Product";
+import "./Desserts.css";
 
 export default function Desserts() {
   return (
-    <div>
+    <div className="Desserts_items">
       <h1>Desserts</h1>
-      <div className='Products'> <Products /></div>
+      <div className="Products">
+        <Products />
+      </div>
     </div>
   );
 }
